4b26329e7d7262976259089add5b93a2773c9c9a
Yaniv-Sh <YanivShnur@gmail.com>
2021-08-01 22:59:01 +0300

    menu + read .txt file added

--- main.cpp
+++ main.cpp
@@ -8,10 +8,39 @@
    ╚═╝   ╚═╝  ╚═╝   ╚═╝      ╚══════╝╚═╝  ╚═══╝ ╚═════╝
 
 ***/
-
 #include <iostream>
+#include <fstream>
+#include <string>
+using std::ifstream;
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
 
-int main() {
-    std::cout << "Hello!" << std::endl;
+int main(){
+    string filename;
+    string text;
+    ifstream name;
+    system("Color 0A"); //Background 0=Black , Text A=Green
+    cout << "##########################" << endl;
+    cout << "## Welcome to Txt-Enc ! ##" << endl;
+    cout << "##########################" << endl << endl;
+    cout << "Enter your .txt file path:" << endl;
+    cout << "e.g. \"C:\\123\\myfile.txt\" "  << endl;
+    cin >> filename;
+    name.open(filename);
+    while (!name.is_open())
+    {
+        name.clear();
+        cout << "Try Again" << endl;
+        cin >> filename;
+        name.open(filename);
+    }
+    while (true)
+    {
+        getline(name, text);
+        if (name.eof()) break;
+        cout << text << endl;
+    }
     return 0;
 }
