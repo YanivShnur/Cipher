83f4e7a52f5dd0078dc1c8dc2fb6d77ee22efb17
Yaniv-Sh <YanivShnur@gmail.com>
2021-08-03 23:46:45 +0300

    Generated the key square matrix

--- PlayfairCipher.cpp
+++ PlayfairCipher.cpp
@@ -1,17 +1,60 @@
 #include "PlayfairCipher.h"
 
-PlayfairCipher::PlayfairCipher(int key) {
+PlayfairCipher::PlayfairCipher(string key) {
+    setKey(key);
+
+    int len = key.length();
+    string new_key = key;
+    for(int k=0; k<len; ++k)
+    {
+        if(new_key[k]=='j')
+            new_key[k]='i';
+    }
+
+    abc['j'-FIRST_LET] = true;
+
+    int idx = 0, index = 0;
+    int i = 0, j = 0;
+    for(i=0; i<MAT_SIZE; ++i)
+    {
+        for(j=0; j<MAT_SIZE; ++j)
+        {
+            if(idx<len) {
+                if (abc[new_key[idx] - FIRST_LET] == false) {
+                    table[i][j] = new_key[idx];
+                    abc[new_key[idx] - FIRST_LET] = true;
+                }
+                ++idx;
+            }
+            else{
+                while(abc[index] == true)
+                {
+                    ++index;
+                }
+                table[i][j] = FIRST_LET + index;
+                ++index;
+            }
+        }
+    }
 }
 
 PlayfairCipher::~PlayfairCipher() {
 }
 
+void PlayfairCipher::setKey(const string k) {
+    PlayfairCipher::key = k;
+}
+
+string PlayfairCipher::getKey() const {
+    return PlayfairCipher::key;
+}
+
 string PlayfairCipher::encrypt(const string& str) {
-    string encrypted;
+    string encrypted = str;
     return encrypted;
 }
 
 string PlayfairCipher::decrypt(const string& str) {
-    string decrypted;
+    string decrypted = str;
     return decrypted;
 }
--- PlayfairCipher.h
+++ PlayfairCipher.h
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <string>
+#define LET_NUM 26 // sum of alphabet lowercase letters
+#define FIRST_LET 'a' // first alphabet lowercase letter
+#define END_LET 'z' // last alphabet lowercase letter
+#define MAT_SIZE 5
 using std::string;
 using std::cout;
 using std::endl;
 
 class PlayfairCipher {
+    string key = "";
+    bool abc[LET_NUM] = { false };
+    char table[MAT_SIZE][MAT_SIZE] = {};
 public:
-    PlayfairCipher(int key);
+    PlayfairCipher(string key);
     ~PlayfairCipher();
+    string getKey() const;
+    void setKey(const string key);
     string encrypt(const string&); // get string message and return it after encrypt
     string decrypt(const string&); // get string message and return it after decrypt
 };
