a345c43f8b819b2f2acd07471ccd25d7c96f2fa3
Yaniv-Sh <YanivShnur@gmail.com>
2021-08-05 21:30:39 +0300

    reduce code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
+#include <limits>
 #include "CaesarCipher.h"
 #include "VigenereCipher.h"
 #include "RailfenceCipher.h"
 #include "PlayfairCipher.h"
 #include "BeaufortCipher.h"
 #include "AutokeyCipher.h"
-#include <limits>
+
 using std::ifstream;
 using std::string;
 using std::cout;
@@ -29,7 +30,8 @@ void logo(){
 
 string stringToLower(string str) {
     int to_sub = 'A' - 'a';
-    for(int i=0; i<str.length(); ++i)
+    int length = str.length();
+    for(int i=0; i<length; ++i)
     {
         if(str[i] <= 'Z' && str[i] >= 'A')
         {
@@ -41,7 +43,8 @@ string stringToLower(string str) {
 
 string stringSpacesRemove(string str) {
     string str_new;
-    for(int i=0; i<str.length(); ++i)
+    int length = str.length();
+    for(int i=0; i<length; ++i)
     {
         if(str[i] != ' ')
         {
@@ -51,184 +54,160 @@ string stringSpacesRemove(string str) {
     return str_new;
 }
 
+string enterMsg(string selection_str){
+    cout << endl;
+    cout << "###################################" << endl;
+    cout << selection_str << endl;
+    cout << "###################################" << endl << endl;
+
+    string msg;
+    cout << "Enter your message and then press the ENTER key:" << endl;
+    cin.ignore(1, '\n');
+    getline(cin, msg);
+    return msg;
+}
+
+bool checkAlpha(string key, const string& checkLength)
+{
+    if(!checkLength.empty())
+    {
+        if (checkLength.length()!=key.length())
+            return false;
+    }
+    for(int i=0; i<key.length(); ++i)
+    {
+        if( !isalpha(key[i]))
+            return false;
+    }
+    return true;
+}
+
+string getStringKey(const string& checkLength = "")
+{
+    string key;
+    cout << endl << "Enter your string key:" << endl;
+    getline(cin, key);
+    while( !checkAlpha(key, checkLength))
+    {
+        cin.clear();
+        cout << endl << "Invalid length! Please enter a valid string:" << endl;
+        getline(cin, key);
+    }
+    return key;
+}
+
+template<typename T>
+void cipherMsg(T Cipher, string msg) {
+    string new_str = Cipher.encrypt(msg);
+    cout << endl << "Encrypted message: " << new_str << endl << endl;
+    new_str = Cipher.decrypt(new_str);
+    cout << "Decrypted message: " << new_str << endl;
+}
+
 void menu(){
-    cout << "##################################" << endl;
-    cout << "##          Cipher Menu         ##" << endl;
-    cout << "##################################" << endl;
-    cout << "## Select one of the following: ##" << endl;
-    cout << "##    1. Caesar Cipher          ##" << endl;
-    cout << "##    2. Vigenere Cipher        ##" << endl;
-    cout << "##    3. Railfence Cipher       ##" << endl;
-    cout << "##    4. Playfair Cipher        ##" << endl;
-    cout << "##    5. Beaufort Cipher        ##" << endl;
-    cout << "##    6. Autokey Cipher         ##" << endl;
-    cout << "##    7. Exit                   ##" << endl;
-    cout << "##################################" << endl << endl;
+    cout << "###################################" << endl;
+    cout << "##          Cipher Menu          ##" << endl;
+    cout << "###################################" << endl;
+    cout << "##                               ##" << endl;
+    cout << "##  Select one of the following: ##" << endl;
+    cout << "##                               ##" << endl;
+    cout << "##    1. Caesar Cipher           ##" << endl;
+    cout << "##    2. Vigenere Cipher         ##" << endl;
+    cout << "##    3. Railfence Cipher        ##" << endl;
+    cout << "##    4. Playfair Cipher         ##" << endl;
+    cout << "##    5. Beaufort Cipher         ##" << endl;
+    cout << "##    6. Autokey Cipher          ##" << endl;
+    cout << "##    7. Exit                    ##" << endl;
+    cout << "##                               ##" << endl;
+    cout << "###################################" << endl << endl;
     cout << "Enter selection and then press the ENTER key:" << endl;
-    int cipherSelected = 7;
+    int cipherSelected;
     cin >> cipherSelected;
 
     switch (cipherSelected) {
         case 1:{
-            cout << "##################################" << endl;
-            cout << "##    1. Caesar Cipher          ##" << endl;
-            cout << "##################################" << endl << endl;
+            string msg = enterMsg("##    1. Caesar Cipher           ##");
 
-            string msg;
-            cout << "Enter your message and then press the ENTER key:" << endl;
-            cin.ignore(1, '\n');
-            getline(cin, msg);
-
-            int shiftKey = 0;
-            cout << endl << "Choose a shift key: " << endl;
+            int shiftKey;
+            cout << endl << "Enter your integer key (greater than or equal to 0) "
+                            "and then press the ENTER key:" << endl;
             cin >> shiftKey;
-            while (!cin.good())
+            while (!cin.good() || shiftKey < 0)
             {
                 cin.clear();
                 cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                cout << "Invalid number! Please enter a valid value";
+                cout << "Invalid number! Please enter a valid value" << endl;
                 cin >> shiftKey;
             }
 
             CaesarCipher CaesarCipher(shiftKey);
-            string new_str = CaesarCipher.encrypt(msg);
-            cout << endl << "Encrypted message: " << new_str << endl;
-            new_str = CaesarCipher.decrypt(new_str);
-            cout << "Decrypted message: " << new_str << endl;
-
+            cipherMsg(CaesarCipher, msg);
 
             break;
         }
         case 2:{
-            cout << "##################################" << endl;
-            cout << "##    2. Vigenere Cipher        ##" << endl;
-            cout << "##################################" << endl << endl;
+            string msg = enterMsg("##    2. Vigenere Cipher         ##");
 
-            string msg;
-            cout << "Enter your message and then press the ENTER key:" << endl;
-            cin.ignore(1, '\n');
-            getline(cin, msg);
-
-            string key;
-            cout << endl << "Choose a string key: " << endl;
-            getline(cin, key);
-            while(key.length()!=msg.length())
-            {
-                cin.clear();
-                cout << endl << "Invalid length! Please enter a valid string key (same length as message): " << endl;
-                getline(cin, key);
-            }
+            string key = getStringKey(msg);
 
             VigenereCipher VigenereCipher(key);
-            string new_str = VigenereCipher.encrypt(msg);
-            cout << endl << "Encrypted message: " << new_str << endl;
-            new_str = VigenereCipher.decrypt(new_str);
-            cout << "Decrypted message: " << new_str << endl;
+            cipherMsg(VigenereCipher, msg);
 
             break;
         }
         case 3:{
-            cout << "##################################" << endl;
-            cout << "##    3. Railfence Cipher       ##" << endl;
-            cout << "##################################" << endl << endl;
+            string msg = enterMsg("##    3. Railfence Cipher        ##");
 
-            string msg;
-            cout << "Enter your message and then press the ENTER key:" << endl;
-            cin.ignore(1, '\n');
-            getline(cin, msg);
-
-            int shiftKey = 0;
-            cout << endl << "Choose a shift key: " << endl; //BIGGER THAN 1 !!
+            int shiftKey;
+            cout << endl << "Enter your integer key (greater than 1) "
+                            "and then press the ENTER key:" << endl;
             cin >> shiftKey;
-            while (!cin.good())
+            while (!cin.good() || shiftKey < 1)
             {
                 cin.clear();
                 cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                cout << "Invalid number! Please enter a valid value";
+                cout << "Invalid number! Please enter a valid value:";
                 cin >> shiftKey;
             }
 
             RailfenceCipher RailfenceCipher(shiftKey, msg.length());
-            string new_str = RailfenceCipher.encrypt(msg);
-            cout << endl << "Encrypted message: " << new_str << endl << endl;
-            new_str = RailfenceCipher.decrypt(new_str);
-            cout << "Decrypted message: " << new_str << endl;
+            cipherMsg(RailfenceCipher, msg);
+
             break;
         }
         case 4:{
-            cout << "##################################" << endl;
-            cout << "##    4. Playfair Cipher        ##" << endl;
-            cout << "##################################" << endl << endl;
-
-            string msg;
-            cout << "Enter your message and then press the ENTER key:" << endl;
-            cin.ignore(1, '\n');
-            getline(cin, msg);
-
-            string key;
-            cout << endl << "Choose a string key: " << endl;
-            getline(cin, key);
-
+            string msg = enterMsg("##    4. Playfair Cipher         ##");
             msg = stringSpacesRemove(stringToLower(msg));
-            key = stringSpacesRemove(stringToLower(key));
+
+            string key = getStringKey();
+            key = stringToLower(key);
 
             PlayfairCipher PlayfairCipher(key, msg.length());
-            string new_str = PlayfairCipher.encrypt(msg);
-            cout << endl << "Encrypted message: " << new_str << endl << endl;
-            new_str = PlayfairCipher.decrypt(new_str);
-            cout << "Decrypted message: " << new_str << endl;
+            cipherMsg(PlayfairCipher, msg);
 
             break;
         }
         case 5:{
-            cout << "##################################" << endl;
-            cout << "##    5. Beaufort Cipher        ##" << endl;
-            cout << "##################################" << endl << endl;
-
-            string msg;
-            cout << "Enter your message and then press the ENTER key:" << endl;
-            cin.ignore(1, '\n');
-            getline(cin, msg);
-
+            string msg = enterMsg("##    5. Beaufort Cipher         ##");
             msg = stringSpacesRemove(stringToLower(msg));
 
-            string key;
-            cout << endl << "Choose a string key: " << endl;
-            getline(cin, key);
-
-            key = stringSpacesRemove(stringToLower(key));
+            string key = getStringKey();
+            key = stringToLower(key);
 
             BeaufortCipher BeaufortCipher(key);
-            string new_str = BeaufortCipher.encrypt(msg);
-            cout << endl << "Encrypted message: " << new_str << endl << endl;
-            new_str = BeaufortCipher.decrypt(new_str);
-            cout << "Decrypted message: " << new_str << endl;
+            cipherMsg(BeaufortCipher, msg);
 
             break;
         }
         case 6:{
-            cout << "##################################" << endl;
-            cout << "##    6. Autokey Cipher         ##" << endl;
-            cout << "##################################" << endl << endl;
-
-            string msg;
-            cout << "Enter your message and then press the ENTER key:" << endl;
-            cin.ignore(1, '\n');
-            getline(cin, msg);
-
+            string msg = enterMsg("##    6. Autokey Cipher          ##");
             msg = stringSpacesRemove(stringToLower(msg));
 
-            string key;
-            cout << endl << "Choose a string key: " << endl;
-            getline(cin, key);
-
-            key = stringSpacesRemove(stringToLower(key));
+            string key = getStringKey();
+            key = stringToLower(key);
 
             AutokeyCipher AutokeyCipher(key);
-            string new_str = AutokeyCipher.encrypt(msg);
-            cout << endl << "Encrypted message: " << new_str << endl << endl;
-            new_str = AutokeyCipher.decrypt(new_str);
-            cout << "Decrypted message: " << new_str << endl;
+            cipherMsg(AutokeyCipher, msg);
 
             break;
         }
