719427d259c88a6e2a41efa48a07709da0ddfbc5
Yaniv-Sh <YanivShnur@gmail.com>
2021-08-05 18:13:59 +0300

    finished encryption and decryption

--- AutokeyCipher.cpp
+++ AutokeyCipher.cpp
@@ -2,6 +2,15 @@
 
 AutokeyCipher::AutokeyCipher(string key) {
     setKey(key);
+    int shift = 0;
+    for(int i=0; i<LET_NUM; ++i)
+    {
+        for(int j=0; j<LET_NUM; ++j)
+        {
+            table[i][j] = (j + shift)%LET_NUM + FIRST_LET;
+        }
+        ++shift;
+    }
 }
 
 AutokeyCipher::~AutokeyCipher() {
@@ -17,10 +26,39 @@ string AutokeyCipher::getKey() const {
 
 string AutokeyCipher::encrypt(const string& str) {
     string encrypted = str;
+
+    int str_len = str.length();
+    if (str_len < key.length())
+    {
+        key = key.substr(0, str_len);
+    }
+    else if (str_len > key.length()){
+        key += str.substr(0, str_len - key.length());
+    }
+
+    for(int i=0; i<str.length(); ++i)
+    {
+        encrypted[i] = table[str[i] - FIRST_LET][key[i] - FIRST_LET];
+    }
     return encrypted;
 }
 
 string AutokeyCipher::decrypt(const string& str) {
     string decrypted = str;
+
+    for(int k=0; k<str.length(); ++k)
+    {
+        for(int i=0; i<str.length(); ++i)
+        {
+            for(int j=0; j<LET_NUM; ++j)
+            {
+                if(table[key[i]-FIRST_LET][j] == str[i])
+                {
+                    decrypted[i] = FIRST_LET + j;
+                    break;
+                }
+            }
+        }
+    }
     return decrypted;
 }
